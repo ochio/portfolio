670632f6b3379670bd2d284f4420ce827ca41402
ochi <ocos54ce@gmail.com>
2021-12-27 22:37:47 +0900

    config

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,14 +7,14 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-jodie/gatsby-config.js
-    siteTitle: `Jodie`,
-    siteTitleAlt: `Jodie - Gatsby Starter Portfolio`,
-    siteHeadline: `Jodie - Gatsby Theme from @lekoarts`,
+    siteTitle: `portfolio`,
+    siteTitleAlt: `portfolio`,
+    siteHeadline: `portfolio`,
     siteUrl: `https://jodie.lekoarts.de`,
-    siteDescription: `Image-heavy photography portfolio with colorful accents & customizable pages. Includes adaptive image grids powered by CSS grid and automatic image integration into projects.`,
-    siteLanguage: `en`,
+    siteDescription: `portfolio`,
+    siteLanguage: `ja`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `@ochio1234`,
   },
   plugins: [
     {
